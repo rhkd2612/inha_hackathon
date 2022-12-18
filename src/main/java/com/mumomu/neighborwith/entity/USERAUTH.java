310f10a51caae12f79cf63063b4d87e7bcda9964
Lee Sang Bin <rhkd2612@naver.com>
2022-12-18 23:38:09 +0900

    feat: user entity

---
+++ src/main/java/com/mumomu/neighborwith/entity/USERAUTH.java
@@ -0,0 +1,8 @@
+package com.mumomu.neighborwith.entity;
+
+public enum USER_AUTH {
+    NO_AUTH,
+    PLACE_AUTH,
+    TEL_AUTH,
+    DEED_AUTH,
+}
---
+++ src/main/java/com/mumomu/neighborwith/entity/User.java
@@ -0,0 +1,48 @@
+package com.mumomu.neighborwith.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "user")
+@Builder @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<Postit> postits = new ArrayList<>();
+
+    @OneToMany(mappedBy = "sender", fetch = FetchType.LAZY)
+    private List<Letter> sendLetters = new ArrayList<>();
+
+    @OneToMany(mappedBy = "receiver", fetch = FetchType.LAZY)
+    private List<Letter> receiveLetters = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    private USER_AUTH auth;
+
+    private String name;
+    private String tel;
+    private String buildingAddress;
+    private String userAddress; // 닉네임으로 사용
+    private String deedImage;
+
+    // 받은 모든 쪽지 반환
+    public List<Letter> getTotalLetters(){
+        List<Letter> result = new ArrayList<>();
+        result = sendLetters + receiveLetters;
+
+        return result;
+    }
+}
