c81240ac7e0346d31495bf68f09cb1f9bc2db8db
Lee Sang Bin <rhkd2612@naver.com>
2022-12-19 03:21:58 +0900

    fix: add webconfig

---
+++ src/main/java/com/mumomu/neighborwith/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.mumomu.neighborwith.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .maxAge(3000);
+    }
+}
