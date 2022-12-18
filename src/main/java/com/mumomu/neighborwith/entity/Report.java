fd59f58e604a8a1badf4b1c7af2da566ffd7bad8
Lee Sang Bin <rhkd2612@naver.com>
2022-12-19 00:36:35 +0900

    feat: report entity

--- src/main/java/com/mumomu/neighborwith/entity/Report.java
+++ src/main/java/com/mumomu/neighborwith/entity/Report.java
@@ -9,13 +9,14 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "letter")
-@Builder @Getter
+@Table(name = "report")
+@Builder
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Letter {
+public class Report {
     @Id
-    @Column(name = "letter_id")
+    @Column(name="report_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -23,22 +24,8 @@ public class Letter {
     @JoinColumn(referencedColumnName = "user_id")
     private User sender;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(referencedColumnName = "user_id")
-    private User receiver;
-
-    // Null이여도 되나, 있으면 게시글 반환
-    private String postitTitle;
-
     private Date createTime;
     private String content;
+    private String ownerTel;
     private boolean isAnonymous;
-
-    public void setSender(User sender) {
-        this.sender = sender;
-    }
-
-    public void setReceiver(User receiver) {
-        this.receiver = receiver;
-    }
 }
